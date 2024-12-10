2bae78eb15395277bd3fbbbca93f25d22100e77c
Shruthi <yadandlashruthi97@gmail.com>
2024-12-10 18:57:58 +0530

    checking

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,22 @@ $('.menu-bar a').on('click', function () {
 $('.xmark-icon').on('click', function () {
     $('.quick-links').hide();
 })
+
+$('.xmark-icon').hide();
+$('.menu-bar .menu-tabs').on('click', function () {
+    $('.quick-links').show();
+    $('.quick-links').animate({ top: '5.5rem' });
+    $('.menu-tabs').hide();
+    $('.xmark-icon').show();
+})
+$('.xmark-icon').on('click', function () {
+    $('.quick-links').hide();
+    $('.quick-links').animate({ top: '-6.5rem' });
+    $('.menu-tabs').show();
+    $('.xmark-icon').hide();
+})
+
+$('.quick-links li a').click(function () {
+    $('.quick-links .tabs').removeClass("active ");
+    $(this).addClass("active ");
+});
